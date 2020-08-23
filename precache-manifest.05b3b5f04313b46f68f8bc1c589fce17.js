6adc02aa6af68f697dcecd3a842046faa2dfeaf0
Cndysaru <sarucindy@gmail.com>
2020-08-23 13:49:49 +0400

    Updates

---
+++ precache-manifest.05b3b5f04313b46f68f8bc1c589fce17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991b2fbd89e8c162ee427823e3128e53",
+    "url": "/forex/index.html"
+  },
+  {
+    "revision": "d65ba18cfddec03eea7f",
+    "url": "/forex/static/css/main.4ea649b0.chunk.css"
+  },
+  {
+    "revision": "747f098d2f415d4fdeeb",
+    "url": "/forex/static/js/2.7ce3e265.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/forex/static/js/2.7ce3e265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d65ba18cfddec03eea7f",
+    "url": "/forex/static/js/main.6cef62d8.chunk.js"
+  },
+  {
+    "revision": "afb62b85368429404a9c",
+    "url": "/forex/static/js/runtime-main.229b5593.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/forex/static/media/logo.5d5d9eef.svg"
+  }
+]);
